f7156ad5f4875c14a8119c1fe61d9a3461dad1bc
Thuku Kanyoro <43640181+Kanyoroh@users.noreply.github.com>
2023-12-22 22:17:59 +0300

    FIRST

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  // other configurations...
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
